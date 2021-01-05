3485d348031d35bfa8368b4a6233af4311a67045
u110 <web.u.project@gmail.com>
2021-01-06 00:27:18 +0900

    fix

---
+++ src/main/java/org/embulk/input/pardot/PluginTask.java
@@ -0,0 +1,61 @@
+package org.embulk.input.pardot;
+
+import com.google.common.base.Optional;
+import org.embulk.config.Config;
+import org.embulk.config.ConfigDefault;
+import org.embulk.config.Task;
+
+import java.util.List;
+
+public interface PluginTask extends Task
+{
+    @Config("user_name")
+    String getUserName();
+
+    @Config("password")
+    String getPassword();
+
+    @Config("user_key")
+    @ConfigDefault("null")
+    Optional<String> getUserKey();
+
+    @Config("app_client_id")
+    @ConfigDefault("null")
+    Optional<String> getAppClientId();
+
+    @Config("app_client_secret")
+    @ConfigDefault("null")
+    Optional<String> getAppClientSecret();
+
+    @Config("business_unit_id")
+    @ConfigDefault("null")
+    Optional<String> getBusinessUnitId();
+
+    @Config("created_before")
+    @ConfigDefault("null")
+    Optional<String> getCreatedBefore();
+
+    @Config("created_after")
+    @ConfigDefault("null")
+    Optional<String> getCreatedAfter();
+
+    @Config("fetch_row_limit")
+    @ConfigDefault("200")
+    Optional<Integer> getFetchRowLimit();
+
+    @Config("activity_type_ids")
+    @ConfigDefault("null")
+    Optional<List<Integer>> getActivityTypeIds();
+
+    @Config("prospect_ids")
+    @ConfigDefault("null")
+    Optional<List<Long>> getProspectIds();
+
+    @Config("sort_key")
+    @ConfigDefault("null")
+    Optional<String> getSortKey();
+
+    @Config("sort_order")
+    @ConfigDefault("null")
+    Optional<String> getSortOrder();
+}
--- src/test/java/org/embulk/input/pardot/TestPardotInputPlugin.java
+++ src/test/java/org/embulk/input/pardot/TestPardotInputPlugin.java
@@ -1,5 +1,42 @@
 package org.embulk.input.pardot;
 
+import com.darksci.pardot.api.PardotClient;
+import org.embulk.config.ConfigException;
+import org.embulk.config.ConfigSource;
+import org.embulk.spi.InputPlugin;
+import org.embulk.test.TestingEmbulk;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 public class TestPardotInputPlugin
 {
+    private static final String BASIC_RESOURCE_PATH = "org/embulk/input/pardot/";
+    private static ConfigSource loadYamlResource(TestingEmbulk embulk, String fileName)
+    {
+        return embulk.loadYamlResource(BASIC_RESOURCE_PATH + fileName);
+    }
+
+    @Rule
+    public TestingEmbulk embulk = TestingEmbulk.builder()
+            .registerPlugin(InputPlugin.class, "pardot", PardotInputPlugin.class)
+            .build();
+
+    @Test
+    public void test__getClient__user_key()
+    {
+        ConfigSource config = loadYamlResource(embulk, "config_test_empty_client_secret.yml");
+        PluginTask task = config.loadConfig(PluginTask.class);
+        try {
+            PardotInputPlugin.getClient(task);
+        }
+        catch (ConfigException e) {
+            assertEquals("please set app_client_id, app_client_secret, business_unit_id", e.getMessage());
+            return;
+        }
+        assertTrue(false);
+    }
 }
